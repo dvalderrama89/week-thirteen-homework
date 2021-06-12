c81c18928683e8f152da8ae98fcd08f134684ffd
David Valderrama <dvalderrama08@gmail.com>
2021-06-11 23:15:40 -0700

    Added category model and get all route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,16 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findAll();
+    const categories = categoriesData.map(category => category.get({ plain: true }));
+    res.status(200).json(categories)
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
