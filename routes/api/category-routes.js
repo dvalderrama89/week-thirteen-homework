c000d5d5a3d806837ac6f627cee690ab96cde015
David Valderrama <dvalderrama08@gmail.com>
2021-06-11 23:20:08 -0700

    Added get by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,9 +15,16 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findByPk(req.params.id);
+    const categories = categoriesData.get({ plain: true });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
