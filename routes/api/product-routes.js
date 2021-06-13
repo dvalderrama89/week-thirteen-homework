f7f70c2a0d75ceab51e54da9ae1b1f3667199884
David Valderrama <dvalderrama08@gmail.com>
2021-06-13 00:57:39 -0700

    Created Product model and get routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findAll();
+    const products = productsData.map(product => product.get({ plain: true }));
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findByPk(req.params.id);
+    res.status(200).json(productsData.get({ plain: true }));
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // create new product
